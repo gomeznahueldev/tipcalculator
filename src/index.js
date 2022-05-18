50b46752b225d78d83bcf50ccb987d110d9aa5ed
Nahuel Gómez <ngomezdw@gmail.com>
2022-05-18 18:46:34 -0300

    New file for media queries

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import "./MediaQueries.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
